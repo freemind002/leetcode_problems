89b99b6d1a94be88f04f64d476aa7a2312cfc0d0
freemind002 <freemind002@gmail.com>
2024-05-20 00:46:54 +0800

    feat: leecode題目練習 1. https://leetcode.com/problems/roman-to-integer/description/

---
+++ 13_RomantoInteger/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        tmp_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        s_len = len(s)
+        ans = sum(
+            [
+                -tmp_dic[key]
+                if (index < s_len and tmp_dic[key] < tmp_dic[s[index]])
+                else tmp_dic[key]
+                for index, key in enumerate(s, 1)
+            ]
+        )
+
+        return ans
+
+
+ans = Solution().romanToInt("MCMXCIV")
+print(ans)
