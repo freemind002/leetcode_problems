fc002901851ab2a72b99ba432fc17f62533f6481
freemind002 <freemind002@gmail.com>
2024-05-21 01:11:29 +0800

    style: 修改排版 1.修改排版

--- 1431_KidsWiththeGreatestNumberofCandies/main.py
+++ 1431_KidsWiththeGreatestNumberofCandies/main.py
@@ -5,8 +5,11 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         candy_max = max(candies)
         result = [
-            True if (candy + extraCandies >= candy_max) else False
-            for candy in candies
+            True if (candy + extraCandies >= candy_max) else False for candy in candies
         ]
 
         return result
+
+
+result = Solution().kidsWithCandies([2, 3, 5, 1, 3], 3)
+print(result)
