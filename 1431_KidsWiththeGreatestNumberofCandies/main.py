b6dd7896f5e61eb9582d1fd2c3bdbfe36321cce0
freemind002 <freemind002@gmail.com>
2024-05-20 23:43:01 +0800

    fix: 修改錯誤 1.修改錯誤

--- 1431_KidsWiththeGreatestNumberofCandies/main.py
+++ 1431_KidsWiththeGreatestNumberofCandies/main.py
@@ -5,7 +5,7 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         candy_max = max(candies)
         result = [
-            True if (candy + extraCandies > candy_max) else False
+            True if (candy + extraCandies >= candy_max) else False
             for candy in candies
         ]
 
