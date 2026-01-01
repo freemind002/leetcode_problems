682b2268a1933b70c1e0289cb230a5870c89e72f
freemind002 <freemind002@gmail.com>
2026-01-01 19:55:05 +0800

    style: 修改程式碼 1. 修改程式碼

--- 0000-0099/0001. Two Sum/main.py
+++ 0000-0099/0001. Two Sum/main.py
@@ -3,14 +3,20 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = [
-            [index_01, index_02]
-            for index_01, num_01 in enumerate(nums[:-1])
-            for index_02, num_02 in enumerate(nums[index_01 + 1 :], index_01 + 1)
-            if num_01 + num_02 == target
-        ]
+        # 哈希表用來存儲已經遍歷過的數值及其索引
+        seen = {}
 
-        return result[0] if result else []
+        # 遍歷 nums
+        for index, num in enumerate(nums):
+            complement = target - num  # 計算差值
+
+            if seen.get(complement) is not None:  # 如果差值已經在哈希表中，說明找到結果
+                return [seen[complement], index]
+
+            # 如果沒有找到，紀錄當前數值及其索引
+            seen[num] = index
+
+        return []  # 如果沒有結果，返回空列表
 
 
 result = Solution().twoSum(nums=[3, 2, 4], target=6)
